6b6bced45e2a980eb9a025f7bc2e17500fd5e9da
Ra7eeq1912005 <128977310+Ra7eeq1912005@users.noreply.github.com>
2025-07-08 22:42:39 +0300

    Update EBook.h

--- Project6/EBook.h
+++ Project6/EBook.h
@@ -7,29 +7,28 @@ private:
     string fileType;
 
 public:
-    EBook(string Title, long yearOfPublish, double Price, string fileType,long Quantity) :
-        Book( Title, yearOfPublish, Price,Quantity), fileType(fileType) {
+    EBook(string Title, long yearOfPublish, double Price, string fileType, long Quantity) :
+        Book(Title, yearOfPublish, Price, Quantity), fileType(fileType) {
 
     }
-     bool canBeSendViaEmail()const override{
+    bool canBeSendViaEmail()const override {
         return true;
     }
 
-     double buy(long bookQuantity, const string& email = "", const string& address = "") override {
-         if (bookQuantity > quantity) {
-             throw runtime_error("Quantum book store: Invalid book quantity requested.");
-         }
-          
+    double buy(long bookQuantity, const string& email = "", const string& address = "") override {
+        if (bookQuantity > quantity) {
+            throw runtime_error(" Invalid book quantity requested.");
+        }
 
-         quantity -= bookQuantity;
-         //stock -= bookQuantity;
 
-         send_to_email(email);
-         return price * bookQuantity;
-     }
-     void send_to_email(const string& email) {
-         cout << "Quantum book store: Sending eBook to" << " " << email << " MailService\n";
+        quantity -= bookQuantity;
+        //stock -= bookQuantity;
 
-     }
+        send_to_email(email);
+        return price * bookQuantity;
+    }
+    void send_to_email(const string& email) {
+        cout << " Sending eBook to" << " " << email << " MailService\n";
+
+    }
 };
-
